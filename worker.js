0b0de523c69e9d86d416f57be949357d867a7ca0
samadgod569 <hellohikaisiho124@gmail.com>
2026-02-05 21:20:09 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,36 +1,19 @@
-export default {
-  async fetch(request) {
-    const url = new URL(request.url);
-
-    // Get API key from query params
-    const apiKey = url.searchParams.get("key");
-    if (!apiKey) {
-      return new Response("Missing API key", { status: 400 });
-    }
-
-    // Get values from query params (supports multiple)
-    const values = url.searchParams.getAll("values");
-    
-
-    // Forward request
-    const response = await fetch(
-      "https://api.craftersmc.net/v1/skyblock/bazaar/items",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "X-Api-Key": apiKey,
-        },
-        values: `[${values}]`
-      }
-    );
-
-    // Return API response directly
-    return new Response(response.body, {
-      status: response.status,
+export async function onRequest({ params, env }) {
+  const res = await fetch(
+    `https://api.craftersmc.net/v1/skyblock/bazaar/${params.itemId}/details`,
+    {
       headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  },
-};
+        'X-API-Key': env.CMC_API_KEY || env.cmc_api_key || env['cmc-api-key']
+      }
+    }
+  )
+
+  return new Response(res.body, {
+    status: res.status,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'public, max-age=30'
+    }
+  })
+}
